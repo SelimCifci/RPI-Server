f57038e022ce203bc8ced54c8d27ad9af2a13fe8
SelimCifci <cifcihasanselim@gmail.com>
2024-02-07 19:52:44 +0100

    .

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ pwm = Motor.Motor()
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
+    #s.listen()
     conn, addr = s.accept()
     with conn:
         print(f"Connected by {addr}")
